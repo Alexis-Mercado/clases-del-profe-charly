96365bbab3d5af19c30835f7b6726e55c9561322
2630024@upv.edu.mx <Alexis Javier>
2026-09-25 19:37:13 -0600

    cuarto commit

--- src/understanding_numbers.py
+++ src/understanding_numbers.py
@@ -3,6 +3,8 @@
 """
 los numeros enteros los podemos sumar (+), 
 restar(-), multiplicar(*) y dividir (/)
+division entera //
+potencias **n
 """
 print(2+3)
 print(3-2)
@@ -11,3 +13,43 @@ print(3/2)
 number_1 = 5
 number_2 = 10
 print(number_1 + number_2)
+
+print("---------------------------------------------")
+
+#potencias 
+print(3**2) #3^2
+print(3**3) #3^3
+print(10**6) #10^6
+print(10%2) # modulo (mod)
+
+
+print("---------------------------------------------")
+"numeros flotantes (reales)"
+"""
+son numeros con punto decimal 
+"""
+print(0.1+0.1)
+print(0.2-0.2)
+print(2*0.1)
+print(2*0.2)
+print(2/0.2)  
+
+
+#imprimir la edad de alguien
+age = 34 #variable del tipo int
+#message= "charly tiene " + age + " años." "error cometido: usar strings con variable entero (int)"
+message= "charly tiene " + str(age) + " años."
+print(message)
+
+print("---------------------------------------------")
+
+"TYPE ERROR"
+"""
+TYPE ERROR: python no puede reconocer el tipo 
+de informacion que se esta utilzando
+"""
+message_f = f"charly tiene {age} años."
+print(message_f)
+
+#metodo type
+print(type(age), type("hola"), type(0.54), type(True))
